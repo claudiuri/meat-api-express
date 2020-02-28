db29e7e4fdf648dab123488598cf8ed592a8f945
Cláudio Yuri <claudio.y07@gmail.com>
2020-02-27 22:00:46 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "no-underscore-dangle": "off",
+    "class-methods-use-this": "off",
+    "no-await-in-loop": "off",
+    "import/prefer-default-export": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "ts": "never"
+      }
+    ],
+    "import/no-extraneous-dependencies": [
+      "error", 
+      {
+        "devDependencies": [
+          "**/*.spec.ts",
+          "src/utils/tests/*.ts"
+        ]
+      }
+    ]
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(1001, () => {
+  console.log('Server on')
+});
